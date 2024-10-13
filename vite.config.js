ac3e8b36becfc515d37875bcfcfc5625bb4606a4
Ahmad Sharif <theahmadsharif@gmail.com>
2024-10-13 08:18:35 +0300

    13 October

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react.ecom/",
+  // base: "/react.ecom/",
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: 'jsdom',
